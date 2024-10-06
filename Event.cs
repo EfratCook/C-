6cd0d29277809cc2f2532ea04fef34cfefaa3607
“EfratCook <efratcook@gmail.com>
2024-10-06 17:05:59 +0300

    my commit message

---
+++ Event.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    internal class Event
+    {
+ 
+        public int Id;
+        public string Title;
+        public DateTime start;
+
+        public Event( string title, DateTime start2)
+        {
+            this.Id = Id++;    
+            this.Title = title;
+            this.start = start2;
+
+        }
+        public void setTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void getAllEvents(List<Event> events)
+        {
+            foreach (Event e in events)
+            {
+                Console.WriteLine($"id: {e.Id}, Title: {e.Title}, start: {e.start}");
+            }
+        }
+        public void addEvaent(Event e, List<Event> events)
+        {
+          events.Add(e);    
+
+        }
+        public void updateEvent(int id,List<Event> events)
+        {
+            foreach(Event e in events)
+            {
+                if(e.Id == id)
+                {
+                    e.setTitle("Bar-Mitzva");
+                }
+            }
+
+        }
+        public void deleteEvent(List<Event> events,int id)
+        {
+            foreach (Event e in events)
+            {
+                if (e.Id == id)
+                {
+                    events.Remove(e);
+                   
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+            List<Event>eventList=new List<Event>(); 
+            // יצירת מספר אירועים
+            Event event1 = new Event( "יום הולדת", new DateTime(2023, 12, 25));
+            Event event2 = new Event("פגישה חשובה", new DateTime(2024, 11, 10));
+            Event event3 = new Event(" מסיבה", new DateTime(2020, 01, 5));
+            Event event4 = new Event(" הופעה", new DateTime(2021, 01, 14));
+            Event event5 = new Event(" שיעור בית ", new DateTime(2015, 04, 01));
+            eventList.Add(event1);
+            eventList.Add(event2);
+            eventList.Add(event3);  
+            eventList.Add(event4);
+            event1.getAllEvents(eventList);
+            //event1.updateEvent(0,eventList);
+            event1.getAllEvents(eventList);
+            Console.WriteLine("-------------");
+            event1.getAllEvents(eventList);
+
+            event1.addEvaent(event5, eventList);
+            Console.WriteLine("-------------");
+            event1.getAllEvents(eventList);
+
+
+
+
+
+
+        }
+    }
+}
